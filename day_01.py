7eef019aef78fc3aa8f2256a247b4f3a6c6c2047
Thanh Huynh <46855681+thuynh323@users.noreply.github.com>
2020-12-06 21:25:15 -0500

    add part 2

--- day_01.py
+++ day_01.py
@@ -4,11 +4,33 @@ Day 1 - Part 1
 Find the two entries that sum to 2020 and then multiply those two numbers together.
 """
 
+import numpy as np
+
 result = []
 with open('day_01.txt') as f:
-    lst = [int(i.strip('\n')) for i in list(f)]
-    for x in lst:
-        y = 2020 - x
-        if y in lst:
-            result.append(x*y)
-print(set(result))
+    lst1 = [int(i.strip('\n')) for i in list(f)]
+    lst2 = [2020 - j for j in lst1]
+    for y in lst2:
+        if y in lst1:
+            result.append(y)
+print(f'The two entries are: {result}')
+print(f'Their product is : {np.prod(result)}')
+
+"""
+12/06/2020
+Day 1 - Part 2
+Find the three entries that sum to 2020 and then multiply those three numbers together.
+"""
+import numpy as np
+
+result = []
+with open('day_01.txt') as f:
+    lst1 = [int(i.strip('\n')) for i in list(f)]
+    lst2 = [2020 - j for j in lst1]
+    for x in lst1:
+        for y in lst2:
+            z = y - x
+            if z in lst1:
+                result.append(z)
+print(f'The three entries are: {list(set(result))}')
+print(f'Their product is : {np.prod(list(set(result)))}')
