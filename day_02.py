4e8a4f31a981ef7c6bd0d3d945a86c46a1854cd7
Thanh Huynh <46855681+thuynh323@users.noreply.github.com>
2020-12-06 23:20:10 -0500

    Update day_02.py

--- day_02.py
+++ day_02.py
@@ -3,27 +3,21 @@
 Day 2: Password Philosophy
 """
 
-import pandas as pd
-df = pd.read_csv('day_02.txt',
-                 delim_whitespace=True,
-                 header=None,
-                 names=['condition', 'character', 'password'])
-df['condition_1'] = df['condition'].str.split('-', expand=True)[0].astype(int)
-df['condition_2'] = df['condition'].str.split('-', expand=True)[1].astype(int)
-df.drop('condition', axis=1, inplace=True)
-df['character'] = df['character'].str.strip(':')
-
 #*************************************************************************************************#
 # Part 1: Each line gives the password policy and then the password.                              #
 # The password policy indicates the lowest and highest number of times a given letter must appear #
 # for the password to be valid. How many passwords are valid according to this policies?          #
 #*************************************************************************************************#
 
-df['count'] = [x.count(y)
-               for x, y in zip(df['password'], df['character'])]
-df['valid_1'] = [1 if a <= x <= b else 0
-                for a, x, b in zip(df['condition_1'], df['count'], df['condition_2'])]
-print(f'Number of valid passwords: {df['valid_1'].sum()}')
+def valid_1(lines):
+    count = 0
+    for l in lines:
+        condition, character, password = l.split(' ')
+        condition_1, condition_2 = map(int, condition.split('-'))
+        character = character.strip(':')
+        if condition_1 <= password.count(character) <= condition_2:
+            count += 1
+    print(count)
 
 #*************************************************************************************************#
 # Part 2: Each policy actually describes two positions in the password,                           #
@@ -34,8 +28,18 @@ print(f'Number of valid passwords: {df['valid_1'].sum()}')
 # How many passwords are valid according to the new interpretation of the policies?               #
 #*************************************************************************************************#
 
-df['valid_2'] = [1 if (a[b - 1] == x or a[c - 1] == x) and
-                      (a[b - 1] != a[c - 1]) else 0
-                 for a, b, c, x in zip(df['password'], df['condition_1'],
-                                       df['condition_2'], df['character'])]
-print(f'Number of valid passwords: {df['valid_2'].sum()}')
+def valid_2(lines):
+    count = 0
+    for l in lines:
+        condition, character, password = l.split(' ')
+        condition_1, condition_2 = map(int, condition.split('-'))
+        character = character.strip(':')
+        if (password[condition_1 - 1] == character or password[condition_2 - 1] == character) and \
+           password[condition_1 - 1] != password[condition_2 - 1]:
+            count += 1
+    print(count)
+
+with open('day_02.txt') as f:
+    lst = [x.strip('\n') for x in f.readlines()]
+    print(f'Number of valid passwords according to policy 1: {valid_1(lst)}')
+    print(f'Number of valid passwords according to policy 2: {valid_2(lst)}')
